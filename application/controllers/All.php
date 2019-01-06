f592dd0d073115ecc462a1f834a3fcaa07cb27fe
lypef <lypef@live.com>
2019-01-06 10:37:35 -0600

    work

--- application/controllers/All.php
+++ application/controllers/All.php
@@ -577,17 +577,17 @@ class All extends CI_Controller {
 			<h2>'.$this->input->post('descripcion').'</h2>
 			<a target="_blank" href="'.$this->input->post('url_cont').'"><img src="'.$url_img.'" alt="'.$this->input->post('title').'"></a>
 			<br><br>
-			Tambien puedes ver toda la galeria de '.NameEmpresaID($this->input->post('id_empresa')).' <a href="'.base_url(). 'index.php/all/clients_administrar?id='.$this->input->post('id_empresa').'" target="_blank"> AQUI</a>
+			Tambien puedes ver toda la galeria de '.NameEmpresaID($this->input->post('id_empresa')).' <a href="'.base_url(). 'all/clients_administrar?id='.$this->input->post('id_empresa').'" target="_blank"> AQUI</a>
 			<br><br>
-			O todas nuestras empresas <a href="'.base_url(). 'index.php/all/view_category?id=0&pag=1" target="_blank"> AQUI</a>
+			O todas nuestras empresas <a href="'.base_url(). 'all/view_category?id=0&pag=1" target="_blank"> AQUI</a>
 		';
 		
 		if (mail($to, $subject, $body, $headers))
 		{
-			redirect($url.'?sendmailserviciotrue=true');
+			redirect($url.'?sendmailserviciotrue=true&id='.$this->input->post('id_empresa').'');
 		}else
 		{
-			redirect($url.'?sendmailserviciofalse=false');
+			redirect($url.'?sendmailserviciofalse=false&id='.$this->input->post('id_empresa').'');
 		}		
 	}
 }
